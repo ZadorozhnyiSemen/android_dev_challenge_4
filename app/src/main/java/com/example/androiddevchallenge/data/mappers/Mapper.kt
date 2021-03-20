dd13ac6c2db53c29a3d529dceaa907baf0d00871
Semyon Zadorozhnyy <s.zadorozhnyy@cardsmobile.ru>
2021-03-20 11:58:27 +0300

    Clean. Add list mapping

--- app/src/main/java/com/example/androiddevchallenge/data/mappers/Mapper.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/mappers/Mapper.kt
@@ -5,4 +5,8 @@ interface Mapper<Data, Domain> {
     fun mapToDomain(dataValue: Data): Domain
 
     fun mapToData(domainValue: Domain): Data
+
+    fun mapListToDomain(dataValue: List<Data>): List<Domain>
+
+    fun mapListToData(domainValue: List<Domain>): List<Data>
 }
